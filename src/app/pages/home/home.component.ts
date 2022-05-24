bc39da8b113925da777c580d6f4d96903140afd6
Matias Diaz <mdiaz@alquilando.com>
2022-05-23 21:06:40 -0300

    Se implemento la integración con animaciones en lottie y se maqueto la home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  file: string = `/assets/animation2.json`;
 
 }
