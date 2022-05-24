4631451c413feae31a9b44f5f140b53059b5df4e
Matias Diaz <mdiaz@alquilando.com>
2022-05-23 21:14:53 -0300

    Se agregaron los componentes compartidos que se van a utilizar

---
+++ src/app/shared/components/main-box/main-box.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-box',
+  templateUrl: './main-box.component.html'
+})
+export class MainBoxComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,10 +12,15 @@ const Modules = [
 ];
 
 import { HeaderComponent } from './components/header/header.component';
-
+import { MainBoxComponent } from './components/main-box/main-box.component';
+import { MobileModalComponent } from './components/mobile-modal/mobile-modal.component';
+import { MobileNavComponent } from './components/mobile-nav/mobile-nav.component';
 
 const Components = [
-    HeaderComponent
+    HeaderComponent,
+    MainBoxComponent,
+    MobileModalComponent,
+    MobileNavComponent
 ];
 
 @NgModule({
