5176673193ee535c897e0af6ddd4bf886b7d9a87
Matias Diaz <mdiaz@alquilando.com>
2022-05-25 17:46:52 -0300

    Arreglos de css en la animacion de home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
