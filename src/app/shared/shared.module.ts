6876fabea88b7b8713290600c27cb9572c0be9a4
Matias Diaz <mdiaz@alquilando.com>
2022-07-09 21:01:31 -0300

    avances en la maqueta

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 const AngularModules = [
   CommonModule,
-  RouterModule
+  RouterModule,
+  HttpClientModule
 ];
 
 const Modules = [
